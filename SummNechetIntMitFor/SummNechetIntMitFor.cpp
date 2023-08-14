7181a2d3778c20be62cb8ccdca46a9f623f8cbf7
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-14 22:13:51 +0500

    Добавьте файлы проекта.

---
+++ SummNechetIntMitFor/SummNechetIntMitFor.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+    int n, s = 0;
+    cout << "Введите верхнюю границу слагаемых чисел: ";
+    cin >> n;
+    for (int k = 0;k <= n;k++) {
+        if (k % 2 != 0) {
+            s += k;
+        }
+    }
+    cout << "Сумма натуральных нечётных чисел от 0 до " << n << ": " << s << endl;
+    // Задержка окна консоли:
+    system("pause > nul");
+    return 0;
+}
